04257b926fbbfefe217b95df4b6ed255be02d3af
Christina Takara <christina.takara@gmail.com>
2024-04-16 09:07:55 -0500

    added index route and put route

--- server.js
+++ server.js
@@ -19,11 +19,48 @@ app.get('/blog', (req, res) => {
     res.send('Hello, you have reached the blog route')
 });
 
+app.get('/api/product', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({message: error.message});
+    }
+})
+
+app.get('/api/product/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({message: error.message});        
+    }
+})
+
 app.post('/api/product', async (req, res) => {
     // console.log(req.body);
     // res.json(req.body);
     try {
         const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({message: error.message});
+    }
+})
+
+// update the product in the database
+app.put('/api/product/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body, {new: true});
+        // no product in database was updated
+        if (!product) {
+            return res.status(404).json({message: 'cannot find product'});
+        } 
         res.status(200).json(product);
     } catch (e) {
         console.error(e);
