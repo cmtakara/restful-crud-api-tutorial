8f7f3f733dc7bf2639aede2230f80920a1c00215
Christina Takara <christina.takara@gmail.com>
2024-04-16 08:47:21 -0500

    created product model and post route

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # restful-crud-api-tutorial
-This application was made following the tutorial below:
+This application was made following the tutorial below:\
 [Build Restful CRUD API with Node.js, Express and MongoDB in 45 minutes for Beginners from Scratch](https://www.youtube.com/watch?v=9OfL9H6AmhQ)
 
 There are differences from what was covered in class.
 
-1. The use of a hardcoded port number, rather than a PORT const in .env
+1. The use of a hardcoded port number, rather than a PORT const in .env [^1]
+2. The use of connection string in the server, rather than in .env [^1]
+3. Nesting the app.listen in the mongoose connection callback, rather than having it sequentially after.
+4. not using /api/ routes. [^2]
+5. using Insomnia rather than Postman
+
+[^1]: I have updated my own code to use the .env, so that my connection string is not pushed to github.
+[^2]: I have updated my own routes to use /api when I want to interact with my database.  This means that the responses are json objects, rather than a user view.
--- server.js
+++ server.js
@@ -1,11 +1,41 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+const mongoURI = process.env.MONGO_URI;
+const Product = require('./models/productModel')
+
+// this middleware is required to be able to accept and parse the json body of our requests
+app.use(express.json())
 
 // routes
 app.get('/', (req, res) => {
     res.send('hello NODE API')
 })
 
-app.listen(3000, () => {
-    console.log('node api app is running on port 3000')
+app.get('/blog', (req, res) => {
+    res.send('Hello, you have reached the blog route')
+});
+
+app.post('/api/product', async (req, res) => {
+    // console.log(req.body);
+    // res.json(req.body);
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({message: error.message});
+    }
 })
+
+mongoose.connect(mongoURI)
+.then(() => {
+    console.log('connected to mongoDB');
+    app.listen(PORT, () => {
+        console.log(`node api app is running on port ${PORT}`)
+    })
+})
+.catch((error) => {console.log(error)})
